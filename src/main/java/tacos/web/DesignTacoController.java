b98f3ac6776adb9d5efa0e987be3969afcde0c11
GavrikGal <gavrik.gal@gmail.com>
2024-06-23 21:58:45 +0300

    Added Spring Data JDBC

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -14,7 +14,6 @@ import tacos.TacoOrder;
 import tacos.data.IngredientRepository;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,19 +35,6 @@ public class DesignTacoController {
     public void addIngredientsToModel(Model model) {
         List<Ingredient> ingredients = new ArrayList<>();
         ingredientRepo.findAll().forEach(ingredients::add);
-
-//        List<Ingredient> ingredients = Arrays.asList(
-//                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-//                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-//                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-//                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-//                new Ingredient("TMTO", "Deced Tomatoes", Type.VEGGIES),
-//                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-//                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-//                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-//                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-//                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-//        );
 
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
@@ -80,7 +66,6 @@ public class DesignTacoController {
         }
 
         tacoOrder.addTaco(taco);
-        log.info("Processing taco: {}", taco);
 
         return "redirect:/orders/current";
     }
--- src/main/java/tacos/web/IngredientByIdConverter.java
+++ src/main/java/tacos/web/IngredientByIdConverter.java
@@ -5,10 +5,6 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import tacos.Ingredient;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import tacos.Ingredient.Type;
 import tacos.data.IngredientRepository;
 
 
@@ -26,34 +22,4 @@ public class IngredientByIdConverter implements Converter<String, Ingredient> {
     public Ingredient convert(String id) {
         return ingredientRepo.findById(id).orElse(null);
     }
-
-//    private final Map<String, Ingredient> ingredientMap = new HashMap<>();
-//
-//    public IngredientByIdConverter() {
-//        ingredientMap.put("FLTO",
-//                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-//        ingredientMap.put("COTO",
-//                new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-//        ingredientMap.put("GRBF",
-//                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-//        ingredientMap.put("CARN",
-//                new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-//        ingredientMap.put("TMTO",
-//                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-//        ingredientMap.put("LETC",
-//                new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-//        ingredientMap.put("CHED",
-//                new Ingredient("CHED", "Cheddar", Type.CHEESE));
-//        ingredientMap.put("JACK",
-//                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-//        ingredientMap.put("SLSA",
-//                new Ingredient("SLSA", "Salsa", Type.SAUCE));
-//        ingredientMap.put("SRCR",
-//                new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-//    }
-//
-//    @Override
-//    public Ingredient convert(String id) {
-//        return ingredientMap.get(id);
-//    }
 }
