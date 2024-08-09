5002906e3fca68dc0e728b823f2097c1f7613301
GavrikGal <gavrik.gal@gmail.com>
2024-08-10 01:32:09 +0300

    Start migration to multi-module Spring project. Added Data-module

---
+++ tacocloud-data/src/main/java/tacos/data/OrderRepository.java
@@ -0,0 +1,15 @@
+package tacos.data;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import tacos.TacoOrder;
+import tacos.User;
+
+import java.util.List;
+
+
+public interface OrderRepository
+        extends CrudRepository<TacoOrder, Long> {
+
+    List<TacoOrder> findByUserOrderByIdDesc(User user, Pageable pageable);
+}
