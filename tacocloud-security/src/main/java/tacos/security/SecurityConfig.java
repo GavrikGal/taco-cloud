1a01786bbaae7d41f04669873c469d8cac7b5568
GavrikGal <gavrik.gal@gmail.com>
2024-08-12 22:58:18 +0300

    Start migration to multi-module Spring project. tacocloud-security ready

---
+++ tacocloud-security/src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package tacos.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+//@EnableMethodSecurity
+public class SecurityConfig{
+
+//    @Autowired
+//    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/ingredients").permitAll()
+                .requestMatchers("/api/tacos", "api/ingredients").permitAll()
+                .requestMatchers("/**").permitAll())
+                .formLogin(login -> login.loginPage("/login"))
+                .httpBasic(httpSecurityHttpBasicConfigurer ->
+                        httpSecurityHttpBasicConfigurer.realmName("Taco Cloud"))
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .csrf(httpSecurityCsrfConfigurer ->
+                        httpSecurityCsrfConfigurer
+                                .ignoringRequestMatchers("/h2-console/**", "/api/**"))
+                .headers(httpSecurityHeadersConfigurer ->
+                        httpSecurityHeadersConfigurer.frameOptions(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .userDetailsService(userDatailsService)
+//                .passwordEncoder(encoder());
+//    }
+}
