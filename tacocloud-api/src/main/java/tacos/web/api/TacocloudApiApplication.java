98d0cb6f6dab9b5917704fd4605ee8bc0d35ae81
GavrikGal <gavrik.gal@gmail.com>
2024-08-15 23:33:04 +0300

    Start adding jms

--- tacocloud-api/src/main/java/tacos/web/api/TacocloudApiApplication.java
+++ tacocloud-api/src/main/java/tacos/web/api/TacocloudApiApplication.java
@@ -3,7 +3,7 @@ package tacos.web.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"tacos.data", "tacos.messaging"})
 public class TacocloudApiApplication {
 
 	public static void main(String[] args) {
--- tacocloud-security/src/main/java/tacos/security/SecurityConfig.java
+++ tacocloud-security/src/main/java/tacos/security/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET).permitAll()
                 .requestMatchers(HttpMethod.OPTIONS).permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/ingredients").permitAll()
                 .requestMatchers("/api/tacos", "api/ingredients").permitAll()
